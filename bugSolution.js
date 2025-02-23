6b367d326da6f5fd0ef802b6586c573eea700396
BugHunter-X <askzenai@gmail.com>
2025-02-23 14:32:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,10 @@
+```javascript
+// pages/api/hello.js
+export default function handler(req, res) {
+  if (req.method === 'GET') {
+    res.status(200).json({ name: 'John Doe' });
+  } else {
+    res.status(405).json({ message: 'Method Not Allowed' });
+  }
+}
+```
